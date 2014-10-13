b6060fe1dd9cff59b0c1b31242770635d1c2f9c5
Andrey <andrey@shigapov.net>
2014-10-12 20:37:22 -0700

    - verify the target project file does not exist in order to avoid accidental overwrite

--- export.cmd
+++ export.cmd
@@ -2,5 +2,12 @@
 set CMD_7ZIP="C:\Program Files\7-Zip\7z.exe"
 set PROJECT_NAME=WorldClass
 
-move /Y ".\..\%PROJECT_NAME%.ev3" ".\..\%PROJECT_NAME%.ev3.bak"
+IF EXIST ".\..\%PROJECT_NAME%.ev3" (
+  copy /Y ".\..\%PROJECT_NAME%.ev3" ".\..\%PROJECT_NAME%.ev3.bak" >nul
+  echo ERROR! ".\..\%PROJECT_NAME%.ev3" is already exist, delete or move it to other location before you proceed.
+  goto exit
+)
+
 %CMD_7ZIP% a -tzip ".\..\%PROJECT_NAME%.ev3" ".\xml\%PROJECT_NAME%\*.*"
+
+:exit
