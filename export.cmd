1877f10659fefe2b369840d8984217475cf9a138
endysh <andrey@shigapov.net>
2014-10-12 21:30:06 -0700

    add checks for 7-Zip application

--- export.cmd
+++ export.cmd
@@ -1,6 +1,11 @@
 @echo off
-set CMD_7ZIP="C:\Program Files\7-Zip\7z.exe"
+set CMD_7ZIP=C:\Program Files\7-Zip\7z.exe
 set PROJECT_NAME=WorldClass
+
+IF NOT EXIST "%CMD_7ZIP%" (
+  echo ERROR! 7-Zip application was not found. Please, install it to "C:\Program Files\7-Zip" folder
+  goto exit
+)
 
 IF EXIST ".\..\%PROJECT_NAME%.ev3" (
   copy /Y ".\..\%PROJECT_NAME%.ev3" ".\..\%PROJECT_NAME%.ev3.bak" >nul
@@ -8,6 +13,6 @@ IF EXIST ".\..\%PROJECT_NAME%.ev3" (
   goto exit
 )
 
-%CMD_7ZIP% a -tzip ".\..\%PROJECT_NAME%.ev3" ".\xml\%PROJECT_NAME%\*.*"
+"%CMD_7ZIP%" a -tzip ".\..\%PROJECT_NAME%.ev3" ".\xml\%PROJECT_NAME%\*.*"
 
 :exit
--- import.cmd
+++ import.cmd
@@ -1,7 +1,11 @@
 @echo off
-set CMD_7ZIP="C:\Program Files\7-Zip\7z.exe"
+set CMD_7ZIP=C:\Program Files\7-Zip\7z.exe
 set PROJECT_NAME=WorldClass
 
+IF NOT EXIST "%CMD_7ZIP%" (
+  echo ERROR! 7-Zip application was not found. Please, install it to "C:\Program Files\7-Zip" folder
+  goto exit
+)
 
 IF NOT EXIST ".\..\%PROJECT_NAME%.ev3" (
   echo ERROR! Project file ".\..\%PROJECT_NAME%.ev3" was not found
@@ -10,7 +14,7 @@ IF NOT EXIST ".\..\%PROJECT_NAME%.ev3" (
 
 REM extract XML files
 del /F /S /Q ".\xml\%PROJECT_NAME%\*.*"
-%CMD_7ZIP% e -tzip ".\..\%PROJECT_NAME%.ev3"  -o".\xml\%PROJECT_NAME%"
+"%CMD_7ZIP%" e -tzip ".\..\%PROJECT_NAME%.ev3"  -o".\xml\%PROJECT_NAME%"
 
 REM copy/replace MINDSTORM project
 copy /Y ".\..\%PROJECT_NAME%.ev3" ".\projects\%PROJECT_NAME%.ev3"
