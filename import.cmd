48e299d885538eeb424b6484b96bc6ac67818955
Andrey <andrey@shigapov.net>
2014-10-12 20:38:20 -0700

    - verify the input file project exists

--- import.cmd
+++ import.cmd
@@ -1,7 +1,12 @@
-@echo on
+@echo off
 set CMD_7ZIP="C:\Program Files\7-Zip\7z.exe"
 set PROJECT_NAME=WorldClass
 
+
+IF NOT EXIST ".\..\%PROJECT_NAME%.ev3" (
+  echo ERROR! Project file ".\..\%PROJECT_NAME%.ev3" was not found
+  goto exit
+)
 
 REM extract XML files
 del /F /S /Q ".\xml\%PROJECT_NAME%\*.*"
@@ -9,3 +14,5 @@ del /F /S /Q ".\xml\%PROJECT_NAME%\*.*"
 
 REM copy/replace MINDSTORM project
 copy /Y ".\..\%PROJECT_NAME%.ev3" ".\projects\%PROJECT_NAME%.ev3"
+
+:exit
